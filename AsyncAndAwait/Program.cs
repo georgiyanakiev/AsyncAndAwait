31482a5a1e941f4d2d2403493eb0767f7a4c7e3f
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2020-01-03 16:32:08 +0000

    Add project files.

---
+++ AsyncAndAwait/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace AsyncAndAwait
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            string result = DownloadContent().Result;
+            Console.WriteLine(result);
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+        }
+
+        public static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("http://www.microsoft.com");
+                return result;
+            }
+        }
+    }
+}
